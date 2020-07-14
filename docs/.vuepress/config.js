bc4ae1afa99a0adf980a2dccbaf0c63f3816f8b3
guozhen <guozhen4213@126.com>
2020-07-14 19:16:12 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  title: '文档',
+  description: '学习资料集合',
+  head: [
+    ['link', { rel: 'icon', href: './public/logo.png'}]
+  ],
+  markdown: {
+    lineNumbers: true
+  },
+  evergreen: true,
+  themeConfig: {
+    // logo: '/public/logo.png'
+    // navbar: false,  // 禁用导航栏
+    nav: [
+      { text: '首页', link: '/'},
+      { 
+        text: '前端',
+        items: [
+          { text: 'Vue', link: '/frontend/vue/' },
+          { text: 'React', link: '/frontend/react/' }
+        ]
+      },
+      { text: 'GitHub', link: 'https://github.com/gzh4213', target: '_blank', rel: 'noopener noreferrer'}
+    ],
+    sidebar: 'auto'
+  },
+  themeConfig: {
+    search: true,
+    searchMaxSuggestions: 10
+  },
+  // lastUpdated: 'Last Updated', // string | boolean
+}
