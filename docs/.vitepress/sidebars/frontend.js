9fbaa39945244e8eb400a3b7f766c20151e2bdf3
guozhen <guozhen4213@126.com>
2023-11-24 23:04:45 +0800

    feat: a

--- docs/.vitepress/sidebars/frontend.js
+++ docs/.vitepress/sidebars/frontend.js
@@ -4,7 +4,6 @@ import react from './modules/react'
 import ts from './modules/ts'
 import specification from './modules/specification'
 import data from './modules/data'
-import interview from './modules/interview'
 import build from './modules/build'
 import eslint from './modules/eslint'
 
@@ -15,7 +14,6 @@ export default [
   ...ts,
   ...specification,
   ...data,
-  ...interview,
   ...build,
   ...eslint
 ]
